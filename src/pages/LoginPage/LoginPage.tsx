011e7cc8974de3ea0550bdcb7ec651c9fb0e6ee7
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-02-03 09:20:54 +0200

    created loginPage

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,11 @@
+import "./loginPage.scss";
+
+const LoginPage = () => {
+  return (
+    <div className="login_wrap">
+      <div className="login_header">LoginPage</div>
+    </div>
+  );
+};
+
+export default LoginPage;
