6b29b4aaa9ea100c4c9087b311fb3d64b3032e75
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-02-06 15:41:46 +0200

    adduser message

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,21 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IUserMessage } from "./types";
+
+const initialState: IUserMessage = {
+  message: "",
+};
 
 const userSlice = createSlice({
   name: "user",
-  initialState: {},
+  initialState,
   reducers: {
     createUser: (state, action) => {
       // Этот экшн вызывается из компонента для начала процесса создания пользователя
     },
     createUserSuccess: (state) => {
       // Этот экшн вызывается в саге при успешной отправке данных на бекенд
+      state.message = "Замовлення успішно відправлено";
     },
     createUserFailure: (state, action) => {
       // Этот экшн вызывается в саге в случае ошибки при отправке данных на бекенд
+      state.message = "Нажаль замовлення не відправлено";
       console.error("Create user failed:", action.payload);
     },
   },
 });
 
-export const { createUser, createUserSuccess, createUserFailure } = userSlice.actions;
+export const { createUser, createUserSuccess, createUserFailure } =
+  userSlice.actions;
 export default userSlice.reducer;
