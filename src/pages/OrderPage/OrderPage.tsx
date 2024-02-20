80050e14abc168ba36bd04f4be516ac5b1d08711
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-02-20 19:53:25 +0200

    added styles to orderPage

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -13,36 +13,46 @@ const OrderPage: FC = () => {
 
   return (
     <div className="order_wrap">
-      <fieldset>
-        <legend>Меню 1</legend>
-        <ol>
-          {dalyMenu &&
-            Object.values(dalyMenu.menu1).map((menuItem, index) => {
-              return (
+      {dalyMenu && (
+        <>
+          <fieldset>
+            <legend>Меню 1</legend>
+            <ol>
+              {Object.values(dalyMenu.menu1).map((menuItem, index) => (
                 <li key={index}>
-                  <p>{menuItem.meal}</p>
+                  <p>
+                    <span>{index + 1}.</span> {menuItem.meal}
+                  </p>
                   <img src={menuItem.image} alt="dishImage" />
                 </li>
-              );
-            })}
-          {/* {firstImage &&
-            Array.from(new Set(firstImage.filter((item) => item !== ""))).map(
-              (imgItem, index) => <img key={index} src={imgItem}/>
-            )} */}
-        </ol>
-      </fieldset>
-      <fieldset>
-        <legend>Меню 2</legend>
-        <ol>
-          {/* {Object.values(dalyMenu.menu2).map((menuItem, index) => {
-            return <li key={index}>{menuItem}</li>;
-          })} */}
-        </ol>
-      </fieldset>
-      <fieldset>
-        <legend>Десерт</legend>
-        {/* {<p>{dalyMenu.dessert}</p>} */}
-      </fieldset>
+              ))}
+            </ol>
+          </fieldset>
+
+          <fieldset>
+            <legend>Меню 2</legend>
+
+            <ol>
+              {Object.values(dalyMenu.menu2).map((menuItem, index) => (
+                <li key={index}>
+                  <p>
+                    <span>{index + 1}.</span> {menuItem.meal}
+                  </p>
+                  <img src={menuItem.image} alt="dishImage" />
+                </li>
+              ))}
+            </ol>
+          </fieldset>
+
+          <fieldset>
+            <legend>Десерт</legend>
+            <div className="dessert_wrap">
+              <p>{dalyMenu.bigDessert.meal}</p>
+              <img src={dalyMenu.bigDessert.image} alt="dishImage" />
+            </div>
+          </fieldset>
+        </>
+      )}
     </div>
   );
 };
