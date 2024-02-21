77779e2779c47472957510f0bdbcaa01248a5e4a
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-02-21 11:47:24 +0200

    added submit button

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -15,42 +15,44 @@ const OrderPage: FC = () => {
     <div className="order_wrap">
       {dalyMenu && (
         <>
-          <fieldset>
-            <legend>Меню 1</legend>
-            <ol>
-              {Object.values(dalyMenu.menu1).map((menuItem, index) => (
-                <li key={index}>
-                  <p>
-                    <span>{index + 1}.</span> {menuItem.meal}
-                  </p>
-                  <img src={menuItem.image} alt="dishImage" />
-                </li>
-              ))}
-            </ol>
-          </fieldset>
-
-          <fieldset>
-            <legend>Меню 2</legend>
-
-            <ol>
-              {Object.values(dalyMenu.menu2).map((menuItem, index) => (
-                <li key={index}>
-                  <p>
-                    <span>{index + 1}.</span> {menuItem.meal}
-                  </p>
-                  <img src={menuItem.image} alt="dishImage" />
-                </li>
-              ))}
-            </ol>
-          </fieldset>
-
-          <fieldset>
-            <legend>Десерт</legend>
-            <div className="dessert_wrap">
-              <p>{dalyMenu.bigDessert.meal}</p>
-              <img src={dalyMenu.bigDessert.image} alt="dishImage" />
+          <form>
+            <div className="form_body">
+              <fieldset>
+                <legend>Меню 1</legend>
+                <ol>
+                  {Object.values(dalyMenu.menu1).map((menuItem, index) => (
+                    <li key={index}>
+                      <p>
+                        <span>{index + 1}.</span> {menuItem.meal}
+                      </p>
+                      <img src={menuItem.image} alt="dishImage" />
+                    </li>
+                  ))}
+                </ol>
+              </fieldset>
+              <fieldset>
+                <legend>Меню 2</legend>
+                <ol>
+                  {Object.values(dalyMenu.menu2).map((menuItem, index) => (
+                    <li key={index}>
+                      <p>
+                        <span>{index + 1}.</span> {menuItem.meal}
+                      </p>
+                      <img src={menuItem.image} alt="dishImage" />
+                    </li>
+                  ))}
+                </ol>
+              </fieldset>
+              <fieldset>
+                <legend>Десерт</legend>
+                <div className="dessert_wrap">
+                  <p>{dalyMenu.bigDessert.meal}</p>
+                  <img src={dalyMenu.bigDessert.image} alt="dishImage" />
+                </div>
+              </fieldset>
             </div>
-          </fieldset>
+            <button className="submit">Відправити</button>
+          </form>
         </>
       )}
     </div>
