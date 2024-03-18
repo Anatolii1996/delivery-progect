1512e5afa8a8391d4b3399e34e0980a9243f8eb1
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-03-18 10:04:55 +0200

    added style to enter wrap

--- src/pages/EnterPage/EnterPage.tsx
+++ src/pages/EnterPage/EnterPage.tsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import { FC, useState } from "react";
+import { useForm, SubmitHandler, Resolver } from "react-hook-form";
+import { FormValues, ErrorValues } from "./types";
 
-const EnterPage = () => {
+import { message } from "antd";
+import cn from "classnames";
+import "./enter.scss"
+
+const EnterPage: FC = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const resolver: Resolver<FormValues> = async (values) => {
+    const errors: Partial<ErrorValues> = {};
+
+    if (!values.email) {
+      errors.email = {
+        type: "required",
+        message: "* Це поле обов'язкове",
+      };
+    }
+    if (values.email.length > 50) {
+      errors.email = {
+        message: "* Максимальна довжина 50 символів",
+      };
+    }
+
+    if (!values.password) {
+      errors.password = {
+        type: "required",
+        message: "* Це поле обов'язкове",
+      };
+    }
+
+    if (values.password.length < 8 && values.password.length > 0) {
+      errors.password = {
+        message: "*Мінімальна довжина 8 символів",
+      };
+    }
+
+    return {
+      values: values,
+      errors: errors,
+    };
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver });
+
+  const infoClasses = {
+    email: cn({
+      info: formState.email,
+    }),
+    password: cn({
+      info: formState.password,
+    }),
+  };
+
   return (
-    <div>EnterPage</div>
-  )
-}
+    <div className="enter_wrap">
+      {contextHolder}
+      <form
+        className="form"
+        //    onSubmit={handleSubmit(onSubmit)}
+      >
+        <p className="title">Вхід</p>
 
-export default EnterPage
+        <div className="user_box">
+          <input
+            type="email"
+            autoComplete="off"
+            className="input"
+            // {...register("email")}
+            // value={formState.email}
+            // onChange={handleFormChange}
+          />
+          <label
+            className={infoClasses.email}
+          >
+            *Email
+          </label>
+          <p>{errors.email?.message}</p>
+        </div>
+
+        <div className="user_box">
+          <input
+            autoComplete="off"
+            type="password"
+            className="input"
+            // {...register("password")}
+            // value={formState.password}
+            // onChange={handleFormChange}
+          />
+          <label
+            className={infoClasses.password}
+          >
+            *Пароль
+          </label>
+          <p>{errors.password?.message}</p>
+        </div>
+
+        <button className="submit">Зареєструватись</button>
+
+        <a href="#">Забули пароль?</a>
+      </form>
+    </div>
+  );
+};
+
+export default EnterPage;
---
+++ src/pages/EnterPage/types.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export interface FormValues {
+ 
+  email: string;
+  password: string;
+  
+}
+
+export interface ErrorValues {
+ 
+  email: {
+    type?: string;
+    message: string;
+  };
+  password: {
+    type?: string;
+    message: string;
+  };
+  
+}
